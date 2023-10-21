6fa85a8b098268513e1a297daab409cb96314c55
omar <omarfarukchy93@gmail.com>
2023-10-22 04:54:41 +0600

    react project gpt3

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { Brand, Cta, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+const App = () => {
+  return (
+    <div className="app">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
